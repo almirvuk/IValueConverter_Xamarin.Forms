ab2a80f7db31b07064c651229e50aba9e84d72b7
Almir Vuk <almir.vuk@outlook.com>
2017-05-17 11:25:26 +0200

    Add project files.

---
+++ IValueConverter_Xamarin.Forms/IValueConverter_Xamarin.Forms/Converters/ColorConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace IValueConverter_Xamarin.Forms.Converters {
+
+    public class ColorConverter : IValueConverter {
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+
+            if (value as string == "Winter") 
+                return Color.FromHex("#2980b9");
+
+            else if(value as string == "Spring") 
+                return Color.FromHex("#2ecc71");
+
+            else if (value as string == "Summer")
+                return Color.FromHex("#1abc9c");
+
+            else if (value as string == "Autumn")
+                return Color.FromHex("#e67e22");
+
+            return Color.FromHex("#000000");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ IValueConverter_Xamarin.Forms/IValueConverter_Xamarin.Forms/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace IValueConverter_Xamarin.Forms.ViewModels {
+
+    public class MainPageViewModel : INotifyPropertyChanged{
+
+        private string season;
+        public string Season {
+            get { return season; }
+            set { season = value;
+                OnPropertyChanged("Season");
+            }
+        }
+
+        public ICommand WinterSelected { get; private set; }
+        public ICommand SpringSelected { get; private set; }
+        public ICommand SummerSelected { get; private set; }
+        public ICommand AutumnSelected { get; private set; }
+
+        public MainPageViewModel() {
+
+            WinterSelected = new Command(SetWinter);
+            SpringSelected = new Command(SetSpring);
+            SummerSelected = new Command(SetSummer);
+            AutumnSelected = new Command(SetAutumn);
+        }
+
+
+        void SetWinter() {
+            Season = "Winter";
+        }
+
+        void SetSpring() {
+            Season = "Spring";
+        }
+
+        void SetSummer() {
+            Season = "Summer";
+        }
+
+        void SetAutumn() {
+            Season = "Autumn";
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertyChanged([CallerMemberName]string propertyName = "") =>
+              PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
